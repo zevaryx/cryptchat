6d8882145edb93ba162356275dd3a038a0003664
Zevaryx <zevaryx@gmail.com>
2019-11-16 03:35:26 -0700

    [CryptChatProto] Changed filename conventions from .cs to .g.cs for generated code

--- CryptChatProtos/build_win.bat
+++ CryptChatProtos/build_win.bat
@@ -1,8 +1,8 @@
 @echo off
 
-protoc protos\*.proto -Iprotos\ --csharp_out=protos\
-protoc protos\responses\*.proto -Iprotos\responses --csharp_out=protos\responses
-protoc protos\requests\*.proto -Iprotos\requests --csharp_out=protos\requests
+protoc protos\*.proto -Iprotos\ --csharp_out=protos\ --csharp_opt=file_excention=.g.cs
+protoc protos\responses\*.proto -Iprotos\responses --csharp_out=protos\responses --csharp_opt=file_excention=.g.cs
+protoc protos\requests\*.proto -Iprotos\requests --csharp_out=protos\requests --csharp_opt=file_excention=.g.cs
 
 robocopy /move /xo protos\ . *.cs
 robocopy /move /xo protos\requests\ Requests\ *.cs
