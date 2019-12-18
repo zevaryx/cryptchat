8869a061dae09759f73916422ad0b30196a14b99
Casey Burklow <casey.burklow@unco.edu>
2019-12-18 13:51:07 -0700

    [client] Move to Xamarin client for now

---
+++ CryptChat.Client/CryptChat.Client/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using CryptChat.Client.Models;
+
+namespace CryptChat.Client.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
